eb628c253a357a91d62fde36ee77abe3461e7967
Eric Sherman <ericandrewsherman@gmail.com>
2018-06-19 12:36:13 -0500

    expire cache

--- scrape_n_geo/scrape_n_geo/settings.py
+++ scrape_n_geo/scrape_n_geo/settings.py
@@ -98,7 +98,7 @@ LOG_LEVEL = "ERROR"
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
 HTTPCACHE_ENABLED = True
-HTTPCACHE_EXPIRATION_SECS = 0
+HTTPCACHE_EXPIRATION_SECS = 86400
 HTTPCACHE_DIR = "httpcache"
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
